cd21a90f4f7b2dae707d67b2f55df0748f6e7e47
Ayomide Osineye <230416060@aston.ac.uk>
2025-11-06 16:56:40 +0000

    Made folders/files related to my project, starting to implement code for baseline model

---
+++ SmartMatch/Baseline/smartmatchbasev1.py
@@ -0,0 +1,7 @@
+# This is my first draft of my baseline code
+# The code will be object orientated later but in this file
+# I will have the whole process to show the steps I took to implement 
+# the problem 
+
+
+
